f9c99bb644f27ef70b26eb27ae39b480372315c4
carlos roman <carlochess@gmail.com>
2016-10-10 19:45:40 -0500

    Fixture como initial_data

--- webpage/network_lab/settings.py
+++ webpage/network_lab/settings.py
@@ -138,7 +138,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+#FIXTURE_DIRS = (
+#    os.path.join(BASE_DIR, 'network_lab', 'apps', 'blog_engine', 'fixtures'), 
+#)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
