61c5f5499b31101645e08add049d7ff0d9c6a669
Carlos Roman <carlochess@gmail.com>
2016-10-23 11:14:25 -0500

    Agregada la creacion dinámica de cont

--- webpage/network_lab/apps/thesis/urls.py
+++ webpage/network_lab/apps/thesis/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 
-from .views import Index
+from .views import Index, Docker
 urlpatterns = [
     url(r'^$', Index.as_view(), name='index-thesis'),
+    url(r'^create$', Docker.as_view(), name='condor'),
 ]
