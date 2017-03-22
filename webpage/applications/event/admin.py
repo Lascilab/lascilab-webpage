510e8d5783c8190e510caaac3ed8213bbbbf9a49
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2017-03-21 23:31:25 -0500

    Construcción image nginx

--- webpage/applications/event/admin.py
+++ webpage/applications/event/admin.py
@@ -14,6 +14,7 @@ class EventAdmin(admin.ModelAdmin):
     fieldsets = (
         (_('Overview'), 
             {'fields': (
+	        'name',
                 'banner', 
                 'important_notes',
                 'mision',
