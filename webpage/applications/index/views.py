6c4377ec755c7a34099a38e17e0fa158bb33af44
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2017-03-22 02:09:25 -0500

    Stable

--- webpage/applications/index/views.py
+++ webpage/applications/index/views.py
@@ -8,10 +8,14 @@ class Index(TemplateView):
 
     def get_context_data(self,**kwargs):
         context = super(Index,self).get_context_data(**kwargs)
-        group = Group.objects.get(name='Member')
-        members = list(group.person_set.all())
-        shuffle(members)
-        chunk_size = 3
-        paired_members = [members[i:i + chunk_size] for i in range(0, len(members), chunk_size)]
-        context['paired_members'] = paired_members
-        return context
+        try:
+	    group = Group.objects.get(name='Member')
+	    members = list(group.person_set.all())
+            shuffle(members)
+            chunk_size = 3
+            paired_members = [members[i:i + chunk_size] for i in range(0, len(members), chunk_size)]
+            context['paired_members'] = paired_members
+	except Exception:
+            context['paired_members'] = []
+
+	return context
